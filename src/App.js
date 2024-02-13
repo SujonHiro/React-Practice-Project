fed26b6701a0084bbf73fc4309637f19a2b5b7eb
Sujon <57975469+SujonHiro@users.noreply.github.com>
2024-02-13 16:49:24 +0600

    npm issue fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 /* import Accordian from "./components/Accordian/Accordian"; */
 import RandomColor from "./components/random-color/RandomColor";
+import StartRating from './components/star-Rating/StartRating';
 
 function App() {
   return (
     <div>
      {/*  <Accordian /> */}
-     <RandomColor/>
+     {/* <RandomColor/> */}
+     <StartRating noOfStars={10}/>
     </div>
   );
 }
